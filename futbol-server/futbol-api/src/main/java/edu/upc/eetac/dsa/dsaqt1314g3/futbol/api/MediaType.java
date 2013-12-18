dc2c46ef42683878cb7b7d9211ea2d98e56bcd1a
isaacfraile <isfrav@gmail.com>
2013-12-18 12:21:38 +0100

    Commit por dirty

---
+++ futbol-server/futbol-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g3/futbol/api/MediaType.java
@@ -0,0 +1,8 @@
+package edu.upc.eetac.dsa.dsaqt1314g3.futbol.api;
+
+public interface MediaType {
+	public final static String FUTBOL_API_NOTICIA = "application/vnd.futbol.api.noticia+json";
+	public final static String FUTBOL_API_NOTICIAS_COLLECTION = "application/vnd.futbol.api.noticia.collection+json";
+	public final static String FUTBOL_API_ERROR = "application/vnd.futbol.api.error+json";
+	
+}
---
+++ futbol-server/futbol-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g3/futbol/api/NoticiaNotFoundException.java
@@ -0,0 +1,17 @@
+package edu.upc.eetac.dsa.dsaqt1314g3.futbol.api;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+import edu.upc.eetac.dsa.dsaqt1314g3.futbol.api.model.FutbolError;
+
+public class NoticiaNotFoundException extends WebApplicationException {
+	private final static String MESSAGE = "Book not found";
+
+	public NoticiaNotFoundException() {
+		super(Response.status(Response.Status.NOT_FOUND)
+				.entity(new FutbolError(Response.Status.NOT_FOUND
+						.getStatusCode(), MESSAGE))
+				.type(MediaType.FUTBOL_API_ERROR).build());
+	}
+}
