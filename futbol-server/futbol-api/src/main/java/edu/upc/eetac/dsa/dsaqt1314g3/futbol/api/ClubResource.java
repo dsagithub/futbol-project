15698a544682047dd75a8a0761dc3512227bbea6
judit <judit.male@estudiant.upc.edu>
2013-12-19 22:09:57 +0100

    Commit ClubResource.java arreglado

--- futbol-server/futbol-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g3/futbol/api/ClubResource.java
+++ futbol-server/futbol-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g3/futbol/api/ClubResource.java
@@ -6,35 +6,41 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.sql.DataSource;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import edu.upc.eetac.dsa.dsaqt1314g3.futbol.api.DataSourceSPA;
 import edu.upc.eetac.dsa.dsaqt1314g3.futbol.api.model.Club;
 import edu.upc.eetac.dsa.dsaqt1314g3.futbol.api.model.CalendarioCollection;
 import edu.upc.eetac.dsa.dsaqt1314g3.futbol.api.model.ClubCollection;
+import edu.upc.eetac.dsa.dsaqt1314g3.libros.api.BookNotFoundException;
+import edu.upc.eetac.dsa.dsaqt1314g3.libros.api.InternalServerException;
+import edu.upc.eetac.dsa.dsaqt1314g3.libros.api.ServiceUnavailableException;
 
-
-@Path("/")
-public class ClubResource { /*
+@Path("/clubs")
+public class ClubResource {
 	private DataSource ds = DataSourceSPA.getInstance().getDataSource();
-	
+
 	@Context
 	private UriInfo uriInfo;
-	
+
 	@GET
 	@Produces(MediaType.FUTBOL_API_CLUB_COLLECTION)
 	public ClubCollection getclubs(@QueryParam("nombre") String nombre,
 			@QueryParam("offset") String offset,
-			@QueryParam("length") String length)
-	{
+			@QueryParam("length") String length) {
 		if ((offset == null) || (length == null))
-			throw new BadRequestException(
-					"Indica un offset y un length ");
+			throw new BadRequestException("Indica un offset y un length ");
 		int ioffset, ilength, icount = 0;
 		try {
 			ioffset = Integer.parseInt(offset);
@@ -52,61 +58,137 @@ public class ClubResource { /*
 			throw new BadRequestException(
 					"Lenght ha de ser entero igual o mayor a 1.");
 		}
-		
+
 		ClubCollection clubs = new ClubCollection();
-		
+
 		Connection conn = null;
 		try {
 			conn = ds.getConnection();
 		} catch (SQLException e) {
 			throw new ServiceUnavailableException(e.getMessage());
 		}
-		
-		try{
+
+		try {
 			Statement stmt = conn.createStatement();
 			String sql = null;
-			
-			if(nombre != null)
-			{
-				sql = "select * from clubs where nombre like '%" + nombre
-						+ "%' LIMIT " + offset
-						+ "," + length;
-			}
-			else 
-			{
-				slq= "select * from clubs LIMIT" + offset + "," + length;
+
+			if (nombre != null) {
+				sql = "select * from Club where nombre like '%" + nombre
+						+ "%' LIMIT " + offset + "," + length;
+			} else {
+				slq = "select * from Club LIMIT" + offset + "," + length;
 			}
 			ResultSet rs = stmt.executeQuery(sql);
-			while (rs.next()) 
-			{
-				
+			while (rs.next()) {
+
 				Club club = new Club();
 				club.setIdClub(rs.getString("idClub"));
 				club.setNombre(rs.getString("nombre"));
-				//Faltan links!
+				// Faltan links!
 				clubs.addClub(club);
 				icount++;
 			}
 			rs.close();
 			stmt.close();
 			conn.close();
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new InternalServerException(e.getMessage());
 		}
-		
-		//links!
+
+		// links!
 		return clubs;
 	}
-	
+
 	@GET
 	@Path("/{idClub}")
 	@Produces(MediaType.FUTBOL_API_CLUB)
+	public Club getClub(@PathParam("idClub") String idClub){
+		
+		Club club = new Club();
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServiceUnavailableException(e.getMessage());
+		}
+		Statement stmt = null;
+		ResultSet rs = null;
+		try
+		{
+			stmt = conn.createStatement();
+			String sql = "select * from Club where id=" + idClub;
+			rs = stmt.executeQuery(sql);
+			if (rs.next())
+			{
+				club.setIdClub("idClub");
+				club.setNombre("nombre");
+				//addlinks
+			}
+			else
+			{
+				throw new ClubNotFoundException("No existe ningún club con esa id")
+			}
+		}
+		catch (SQLException e) 
+		{
+			throw new InternalServerException(e.getMessage());
+		}
+		 finally 
+		 {
+			try 
+			{
+					stmt.close();
+					conn.close();
+				} catch (SQLException e) {
+					throw new InternalServerException(e.getMessage());
+				}
+			}
+		
+		return club;
+		
+		}
+	
+	@POST
+	@Consumes(MediaType.FUTBOL_API_CLUB)
+	@Produces(MediaType.FUTBOL_API_CLUB)
+	public Club createClub(Club club)
+	{
+		
+		
+		
+		if (club.getNombre().length()>45){
+			throw new BadRequestException("El nombre del club ha de ser menor de 45 carácteres);
+		}
+		
+		Connection conn = null;
+		try{
+			Statement stmt = conn.createStatement();
+			string sql = "insert into Club (nombre) values('"
+			+ club.getNombre()
+			+"')";
+			stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+			ResultSet rs = stmt.getGeneratedKeys();
+			if (rs.next()) {
+				
+			club.setIdClub(rs.getInt(1));
+			//If con links
+			rs.close();
+			stmt.close();
+			conn.close();
+			}
+			else {
+				throw new //;
+			}
+			
+			
+			}
+		 catch (SQLException e) {
+				throw new InternalServerException(e.getMessage());
+			}
+		return club;
+		}
+		
+	}//final del resource
+	
 
-	
-	
-	
-*/	
-	
-	
-} 	
+
