f75db072a15cbda6daa341263e76fb9adcc1b79f
judit <judit.male@estudiant.upc.edu>
2013-12-24 10:46:05 +0100

    ClubResource

--- futbol-server/futbol-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g3/futbol/api/ClubResource.java
+++ futbol-server/futbol-api/src/main/java/edu/upc/eetac/dsa/dsaqt1314g3/futbol/api/ClubResource.java
@@ -8,24 +8,38 @@ import java.sql.Statement;
 import javax.sql.DataSource;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
 
+import edu.upc.eetac.dsa.dsaqt1314g3.futbol.api.DataSourceSPA;
 import edu.upc.eetac.dsa.dsaqt1314g3.futbol.api.model.Club;
+import edu.upc.eetac.dsa.dsaqt1314g3.futbol.api.model.CalendarioCollection;
 import edu.upc.eetac.dsa.dsaqt1314g3.futbol.api.model.ClubCollection;
 
-@Path("/clubs")
+
+
+
+
+@Path("/")
 public class ClubResource {
 	private DataSource ds = DataSourceSPA.getInstance().getDataSource();
 
 	@Context
 	private UriInfo uriInfo;
+	@Context
+	private SecurityContext security;
 
 	@GET
 	@Produces(MediaType.FUTBOL_API_CLUB_COLLECTION)
@@ -68,8 +82,8 @@ public class ClubResource {
 			if (nombre != null) {
 				sql = "select * from Club where nombre like '%" + nombre
 						+ "%' LIMIT " + offset + "," + length;
-			} else {
-				slq = "select * from Club LIMIT" + offset + "," + length;
+			} else  {
+				sql = "select * from Club LIMIT" + offset + "," + length;
 			}
 			ResultSet rs = stmt.executeQuery(sql);
 			while (rs.next()) {
@@ -109,7 +123,7 @@ public class ClubResource {
 		try
 		{
 			stmt = conn.createStatement();
-			String sql = "select * from Club where id=" + idClub;
+			String sql = "select * from Club where idClub=" + idClub;
 			rs = stmt.executeQuery(sql);
 			if (rs.next())
 			{
@@ -119,7 +133,7 @@ public class ClubResource {
 			}
 			else
 			{
-				throw new ClubNotFoundException("No existe ningún club con esa id")
+				throw new ClubNotFoundException("No existe ningún club con esa id");
 			}
 		}
 		catch (SQLException e) 
@@ -146,31 +160,35 @@ public class ClubResource {
 	@Produces(MediaType.FUTBOL_API_CLUB)
 	public Club createClub(Club club)
 	{
-		
+//		if (!security.isUserInRole("administrator"))
+//		{
+//			throw new ForbiddenException("Solo administrador puede realizar esta acción");
+//		}
 		
 		
 		if (club.getNombre().length()>45){
-			throw new BadRequestException("El nombre del club ha de ser menor de 45 carácteres);
+			throw new BadRequestException("El nombre del club ha de ser menor de 45 carácteres");
 		}
 		
 		Connection conn = null;
 		try{
 			Statement stmt = conn.createStatement();
-			string sql = "insert into Club (nombre) values('"
+			String sql = "insert into Club (nombre) values('"
 			+ club.getNombre()
 			+"')";
 			stmt.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
 			ResultSet rs = stmt.getGeneratedKeys();
 			if (rs.next()) {
-				
-			club.setIdClub(rs.getInt(1));
+		// Si no hay lasModified las 2 siguientes lineas sobran
+//			int  clubid = rs.getInt(1);
+//			club.setIdClub(Integer.toString(clubid));
 			//If con links
 			rs.close();
 			stmt.close();
 			conn.close();
 			}
 			else {
-				throw new //;
+				throw new ClubNotFoundException("");
 			}
 			
 			
@@ -180,8 +198,77 @@ public class ClubResource {
 			}
 		return club;
 		}
+	
+	@DELETE
+	@Path("/{idClub}")
+	public void deleteSting(@PathParam("idClub") String id) {
+		Connection conn = null;
 		
-	}//final del resource
+//		if (!security.isUserInRole("administrator"))
+//		{
+//			throw new ForbiddenException("Solo administrador puede realizar esta acción");
+//		}
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServiceUnavailableException(e.getMessage());
+		}
+		Statement stmt = null;
+		String sql;
+		try {
+			stmt = conn.createStatement();
+			sql = "delete from Club where idClub=" + id;
+			int rs2 = stmt.executeUpdate(sql);
+			if (rs2 == 0)
+				throw new ClubNotFoundException("No existe ningún club con esa id");
+
+		} catch (SQLException e) {
+			throw new InternalServerException(e.getMessage());
+		} finally {
+			try {
+				conn.close();
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	@PUT
+	@Path("/{idClub}")
+	@Consumes(MediaType.FUTBOL_API_CLUB)
+	@Produces(MediaType.FUTBOL_API_CLUB)
+	public Club updateClub(@PathParam("idClub") String id, Club club) {
+	
+//		if (!security.isUserInRole("administrator"))
+//		{
+//			throw new ForbiddenException("Solo administrador puede realizar esta acción");
+//		}
+		
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServiceUnavailableException(e.getMessage());
+		}
+		try {
+			Statement stmt = conn.createStatement();
+			String sql = "update Club set Club.nombre='" + club.getNombre()
+					  + "' where Club.idClub=" + id;
+			int rs2 = stmt.executeUpdate(sql);
+			if (rs2 == 0)
+				throw new ClubNotFoundException("No existe equipo para actualizar");
+			
+		
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			throw new InternalServerException(e.getMessage());
+		}
+		return club;
+	}
+	
+	}
+
 	
 
 
